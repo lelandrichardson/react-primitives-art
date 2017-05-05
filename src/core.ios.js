06b4d81229cc5765f4fb40610be9b9fcffa712f5
Leland Richardson <leland.m.richardson@gmail.com>
2017-05-04 21:02:30 -0700

    Initial implementation

---
+++ src/core.ios.js
@@ -0,0 +1,27 @@
+import { ART } from 'react-native';
+
+const {
+  ClippingRectangle,
+  Group,
+  LinearGradient,
+  Path,
+  Pattern,
+  RadialGradient,
+  Shape,
+  Surface,
+  Text,
+  Transform,
+} = ART;
+
+module.exports = {
+  ClippingRectangle,
+  Group,
+  LinearGradient,
+  Path,
+  Pattern,
+  RadialGradient,
+  Shape,
+  Surface,
+  Text,
+  Transform,
+};
---
+++ src/core.web.js
@@ -0,0 +1,25 @@
+import {
+  ClippingRectangle,
+  Group,
+  LinearGradient,
+  Path,
+  Pattern,
+  RadialGradient,
+  Shape,
+  Surface,
+  Text,
+  Transform,
+} from 'react-art';
+
+module.exports = {
+  ClippingRectangle,
+  Group,
+  LinearGradient,
+  Path: Mode.Path,
+  Pattern,
+  RadialGradient,
+  Shape,
+  Surface,
+  Text,
+  Transform,
+};
